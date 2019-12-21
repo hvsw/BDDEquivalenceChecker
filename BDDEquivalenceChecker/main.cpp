c0b7a0d381257136b4eb28bf7bae96f0ea12faf4
Henrique Valcanaia <henriqueindalencio@gmail.com>
2019-12-21 12:01:12 -0300

    Initial Commit

---
+++ BDDEquivalenceChecker/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BDDEquivalenceChecker
+//
+//  Created by Henrique Valcanaia on 2019-12-21.
+//  Copyright © 2019 Henrique Valcanaia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
