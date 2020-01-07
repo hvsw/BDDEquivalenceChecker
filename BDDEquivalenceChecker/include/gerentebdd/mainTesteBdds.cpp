5d2a07694d2d0fecf6b757437812e27c011223e8
Henrique Valcanaia <henriqueindalencio@gmail.com>
2020-01-06 22:54:31 -0300

    Refactored main test

--- BDDEquivalenceChecker/include/gerentebdd/mainTesteBdds.cpp
+++ BDDEquivalenceChecker/include/gerentebdd/mainTesteBdds.cpp
@@ -9,15 +9,9 @@
 //{
 //    gerentebdd g;
 //    set<string> conjunto_variaveis;
-//    nodobdd *nd1=g.create_from_equation("c*(a+b)", conjunto_variaveis);
-//    nodobdd *nd2=g.create_from_equation("c+(!(!a*!b))", conjunto_variaveis);
-//    nodobdd *c=g.create_from_equation("c", conjunto_variaveis);
-//    nodobdd *nd3=g.cofactor(nd1, c, '+');
-//    nodobdd *nd4=g.cofactor(nd2, c, '-');
-//    if (nd3==nd4)
-//       {
-//       cout<<"seems to work"<<endl;
-//       }
-//    system("PAUSE");
+//    nodobdd *nd1=g.create_from_equation("!1+!2", conjunto_variaveis);
+//    nodobdd *nd2=g.create_from_equation("!(1*2)", conjunto_variaveis);
+//    printf("Iguais? %d\n", nd1 == nd2);
+//    
 //    return EXIT_SUCCESS;
 //}
